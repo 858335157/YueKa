86f154634affed1ad4c84c1048137a43c834888f
858335157 <858335157@qq.com>
2018-08-27 12:29:11 +0800

    界面
    界面切换方式由五个include，通过能否显示来切换界面改成五个layout，通过按钮和intent来打开不同界面，使内存占用大大减少，运行更加流畅

--- app/src/main/java/com/yueka/yueka/main.java
+++ app/src/main/java/com/yueka/yueka/main.java
@@ -7,22 +7,14 @@ import android.view.View;
 
 
 public class main extends Activity {
-    private View layout_home;
-    private View layout_chatting;
-    private View layout_forum;
-    private View layout_list;
-    private View layout_recommend;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        layout_home = findViewById(R.id.layout_home);
-        layout_chatting = findViewById(R.id.layout_chatting);
-        layout_forum = findViewById(R.id.layout_forum);
-        layout_list = findViewById(R.id.layout_list);
-        layout_recommend = findViewById(R.id.layout_recommend);
+        setContentView(R.layout.home);
+
     }
 
 
@@ -30,42 +22,22 @@ public class main extends Activity {
     public void navclick(View view){
         switch (view.getId()){
             case R.id.home:
-                layout_home.setVisibility(View.VISIBLE);
-                layout_chatting.setVisibility(View.GONE);
-                layout_forum.setVisibility(View.GONE);
-                layout_list.setVisibility(View.GONE);
-                layout_recommend.setVisibility(View.GONE);
+                setContentView(R.layout.home);
                 break;
             case R.id.chatting:
-                layout_home.setVisibility(View.GONE);
-                layout_chatting.setVisibility(View.VISIBLE);
-                layout_forum.setVisibility(View.GONE);
-                layout_list.setVisibility(View.GONE);
-                layout_recommend.setVisibility(View.GONE);
+                setContentView(R.layout.chatting);
                 break;
 
             case R.id.forum:
-                layout_home.setVisibility(View.GONE);
-                layout_chatting.setVisibility(View.GONE);
-                layout_forum.setVisibility(View.VISIBLE);
-                layout_list.setVisibility(View.GONE);
-                layout_recommend.setVisibility(View.GONE);
+                setContentView(R.layout.forum);
                 break;
 
             case R.id.list:
-                layout_home.setVisibility(View.GONE);
-                layout_chatting.setVisibility(View.GONE);
-                layout_forum.setVisibility(View.GONE);
-                layout_list.setVisibility(View.VISIBLE);
-                layout_recommend.setVisibility(View.GONE);
+                setContentView(R.layout.list);
                 break;
 
             case R.id.recommend:
-                layout_home.setVisibility(View.GONE);
-                layout_chatting.setVisibility(View.GONE);
-                layout_forum.setVisibility(View.GONE);
-                layout_list.setVisibility(View.GONE);
-                layout_recommend.setVisibility(View.VISIBLE);
+                setContentView(R.layout.recommend);
                 break;
 
         }
